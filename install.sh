95d351038135e40f4d9ff449fbc65ee3c719f593
Abhishek Dubey <abhi.dubey011999@gmail.com>
2019-10-21 02:28:55 +0530

    Upgraded to Unity Template
    -v4.4

--- install.sh
+++ install.sh
@@ -1,13 +1,66 @@
-SKIPMOUNT=false
+##########################################################################################
+#
+# Unity Config Script
+# by topjohnwu, modified by Zackptg5
+#
+##########################################################################################
 
-PROPFILE=false
+##########################################################################################
+# Unity Logic - Don't change/move this section
+##########################################################################################
 
-POSTFSDATA=false
+if [ -z $UF ]; then
+  UF=$TMPDIR/common/unityfiles
+  unzip -oq "$ZIPFILE" 'common/unityfiles/util_functions.sh' -d $TMPDIR >&2
+  [ -f "$UF/util_functions.sh" ] || { ui_print "! Unable to extract zip file !"; exit 1; }
+  . $UF/util_functions.sh
+fi
 
-LATESTARTSERVICE=false
+comp_check
 
+##########################################################################################
+# Config Flags
+##########################################################################################
+
+# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
+# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
+# Uncomment SYSOVER if you want the mod to always be installed to system (even on magisk) - note that this can still be set to true by the user by adding 'sysover' to the zipname
+# Uncomment DEBUG if you want full debug logs (saved to /sdcard in magisk manager and the zip directory in twrp) - note that this can still be set to true by the user by adding 'debug' to the zipname
+#MINAPI=21
+#MAXAPI=25
+#DYNLIB=true
+#SYSOVER=true
+#DEBUG=true
+
+# Uncomment if you do *NOT* want Magisk to mount any files for you. Most modules would NOT want to set this flag to true
+# This is obviously irrelevant for system installs. This will be set to true automatically if your module has no files in system
+#SKIPMOUNT=true
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info why you would need this
+
+# Construct your list in the following format
+# This is an example
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here
 REPLACE="
 "
+
+##########################################################################################
+# Custom Logic
+##########################################################################################
+
+# Set what you want to display when installing your module
 
 print_modname() {
   ui_print "-------------------------------------------------"
@@ -27,33 +80,34 @@ print_modname() {
   ui_print "7. Constant framerates when shooting videos (1.2)"
   ui_print "8. Improved Denoising for 30 fps videos     (1.2)"
   ui_print " "
-}
-
-on_install() {
-  ui_print " "
-  ui_print "~ Verifying Device ...                           "
-  custom_variables()
-  device_check()
-  ui_print "~ Extracting module files ...                    "
-  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
-  ui_print "~ Done Extracting ...                            "
-  ui_print "-------------------------------------------------"
+  unity_main # Don't change this line
 }
 
 set_permissions() {
   set_perm_recursive $MODPATH 0 0 0755 0644
+
+  # Note that all files/folders have the $UNITY prefix - keep this prefix on all of your files/folders
+  # Also note the lack of '/' between variables - preceding slashes are already included in the variables
+  # Use $VEN for vendor (Do not use /system$VEN, the $VEN is set to proper vendor path already - could be /vendor, /system/vendor, etc.)
+
+  # Some examples:
+
+  # For directories (includes files in them):
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+
+  # set_perm_recursive $UNITY/system/lib 0 0 0755 0644
+  # set_perm_recursive $UNITY$VEN/lib/soundfx 0 0 0755 0644
+
+  # For files (not in directories taken care of above)
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+
+  # set_perm $UNITY/system/lib/libart.so 0 0 0644
 }
 
-custom_variables() {
-  if [ -f vendor/build.prop ]; then device="/system/build.prop vendor/build.prop"; else device="/system/build.prop"; fi
-    kenzo=$(grep -E "ro.product.device=kenzo|ro.build.product=kenzo" $device)
-    kate=$(grep -E "ro.product.device=kate|ro.build.product=kate" $device)
+# Custom Variables for Install AND Uninstall - Keep everything within this function - runs before uninstall/install
+unity_custom() {
+  ui_print "~ Verifying Device ...                           "
+  device_check "kenzo"|"kate"
 }
 
-device_check() {
-  if [ -n "$kenzo" ] || [ -n "$kate" ]; then
-    return 0
-  else
-    abort "Your device is not kenzo/kate or you are using a modified build.prop"
-  fi
-}
+# Custom Functions for Install AND Uninstall - You can put them here
